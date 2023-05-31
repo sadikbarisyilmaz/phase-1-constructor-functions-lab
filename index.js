03fac61b9db6ed4cce382d7a02c6ac1cb3a555ee
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-05-31 14:03:59 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function Scooter(year, color, model) {
+    this.year = year,
+        this.color = color,
+        this.model = model
+}
+function Driver(name, age, experience) {
+    this.name = name,
+    this.age = age,
+    this.experience = experience
+}
+
+function PickupLocation(adress, city) {
+    this.adress = adress,
+    this.city = city
+}
+const lc = new PickupLocation("a","b")
+console.log(lc)
